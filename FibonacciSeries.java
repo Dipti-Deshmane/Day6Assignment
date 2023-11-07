0308ebe212d932954d556460b06ade1821deb44a
Dipti-Deshmane <itsmedipu17@gmail.com>
2023-11-08 00:06:34 +0530

    Perfect Number program added in Day6 Assignemet

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -4,27 +4,32 @@ import java.util.Scanner;
 
 public class FibonacciSeries {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        System.out.println("Please enter length of series");
-        int length = sc.nextInt();
-        for(int i=0;i<length;i++)
-        {
-            System.out.println(fibonacci(i));
+        System.out.println("Enter the number");
+        int num = sc.nextInt();
+
+        if(perfectNum(num)){
+            System.out.println("This perfect number");
         }
+        else {
+            System.out.println("This is not perfect number");
+        }
+
+
     }
-public static int fibonacci(int num)
-{
-if(num<=1){
-    return num;
+
+    public static boolean perfectNum(int num){
+        if(num<1){
+            return false;
+        }
+        int sum=0;
+        for(int i=0;i<num;i++){
+            if(num%2==0){
+                sum+=i;
+            }
+
+        }
+        return sum==num;
+    }
 }
-return fibonacci(num-1)+fibonacci(num-2);
-}
-
-
-
-}
-
-
-
-
-
