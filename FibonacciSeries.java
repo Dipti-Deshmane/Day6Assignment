678e20712edc97dc56e8ee12b660f6e4043d808b
Dipti-Deshmane <itsmedipu17@gmail.com>
2023-11-08 00:28:52 +0530

    Reverse Number program added in Day6 Assignemet

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -4,24 +4,24 @@ import java.util.Scanner;
 
 public class FibonacciSeries {
     public static void main(String[] args) {
-        int limit = 100; // maximum value to check for prime numbers
-
-        System.out.println("Prime numbers between 1 and " + limit + " are:");
-        for (int i = 2; i <= limit; i++) {
-            if (isPrime(i)) {
-                System.out.print(i + " ");
-            }
-        }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int num = sc.nextInt();
+        int reverseNum = reNum(num);
+        System.out.println("Reversed number: " + reverseNum);
+sc.close();
     }
-    public static boolean isPrime(int number) {
-        if (number <= 1) {
-            return false;
+    public static int reNum(int num){
+
+        int reverseNum=0;
+        while (num!=0)
+        {
+            int reminder = num%10;
+            reverseNum=reverseNum*10+reminder;
+            num=num/10;
+
         }
-        for (int i = 2; i <= Math.sqrt(number); i++) {
-            if (number % i == 0) {
-                return false;
-            }
-        }
-        return true;
+        return reverseNum;
     }
 }
+
